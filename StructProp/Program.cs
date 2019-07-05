1c2d9f04aa5e097f0d4cbcbd8cee76e4e02acac6
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-05 14:19:04 +0200

    Add new files

---
+++ StructProp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StructProp
+{
+    struct MyStruct
+    {
+        private int _number;
+        public int No
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value < 100)
+                    _number = value;
+            }
+        }
+        public void callMethod()
+        {
+            Console.WriteLine("The stored value is: {0}", _number);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program declares a struct with a property, a method, and a private field.");
+            MyStruct callStructure = new MyStruct();
+            callStructure.No = 10;
+            callStructure.callMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
