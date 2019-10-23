3efada5091b2bd8f0ec10cdeb95f152b8597abf6
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-10-23 20:59:43 +0200

    Structure in separate file

---
+++ StructProp/MyStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StructProp
+{
+    class MyStruct
+    {
+        private int _number;
+        public int No
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value < 100)
+                    _number = value;
+            }
+        }
+        public void callMethod()
+        {
+            Console.WriteLine("The stored value is: {0}", _number);
+        }
+    }
+}
--- StructProp/Program.cs
+++ StructProp/Program.cs
@@ -2,26 +2,6 @@
 
 namespace StructProp
 {
-    struct MyStruct
-    {
-        private int _number;
-        public int No
-        {
-            get
-            {
-                return _number;
-            }
-            set
-            {
-                if (value < 100)
-                    _number = value;
-            }
-        }
-        public void callMethod()
-        {
-            Console.WriteLine("The stored value is: {0}", _number);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
